2d3c800e10b961907ec0c7cc2926108a6e5fa158
Adicto969 <rl.juan666@gmail.com>
2020-07-11 15:18:57 -0500

    implement change password in fisrt login

--- lib/Screens/ChangePassword/change_password.dart
+++ lib/Screens/ChangePassword/change_password.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:prestaQi/Screens/ChangePassword/change_password_content.dart';
+import 'package:prestaQi/Services/AuthService.dart';
+import 'package:prestaQi/Services/NavigationService.dart';
+import 'package:prestaQi/Services/SetupService.dart';
 import 'package:prestaQi/Utils/ScreenResponsive.dart';
 import 'package:prestaQi/Utils/HexColor.dart';
 
@@ -12,12 +15,70 @@ class ChangePassword extends StatefulWidget {
 class ChangePasswordState extends State<ChangePassword> {
 
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   ScreenResponsive screen;
+  bool loading = false;
+  String message = '';
+
+  String validPassword(String value) {
+    if (value.isEmpty) {
+      return 'El campo es requerido';
+    }
+
+    if (value.length < 6 || value.length > 10) {
+      return 'La contraseña no es segura';
+    }
+
+    return null;
+  }
+
+  String validConfirmPassword(String value) {
+    if (value.isEmpty) {
+      return 'El campo es requerido';
+    }
+
+    if (value.length < 6 || value.length > 10) {
+      return 'La contraseña no es segura';
+    }
+
+    if (value != this.passwordController.text) {
+      return 'La contraseña no es igual';
+    }
+
+    return null;
+  }
 
   @override
   void initState() {
     super.initState();
     this.screen = new ScreenResponsive(context);
+  }
+
+  void submit() {
+    if (this.formKey.currentState.validate()) {
+      setState(() {
+        this.loading = true;
+      });
+
+      appService<AuthService>().changePassword(this.passwordController.text).then((value) {
+        if (value) {
+          appService<NavigationService>().navigateTo('/auth');
+        } else {
+          this.message = 'Error al actualizar';
+          setState(() {
+            this.loading = false;
+          });
+        }
+      }).catchError((onError) {
+        setState(() {
+          this.message = 'Error al actualizar';
+          this.loading = false;
+        });
+      });
+
+    }
   }
 
   @override
--- lib/Screens/ChangePassword/change_password_content.dart
+++ lib/Screens/ChangePassword/change_password_content.dart
@@ -25,127 +25,162 @@ class ChangePasswordContent extends StatelessWidget {
               child: Icon(Icons.close, color: Colors.white, size: 45),
             ),
           ),
-          ListView(
-            children: [
-              SizedBox(
-                height: 100,
-              ),
-              Center(
-                child: Container(
-                  child: Image.asset('assets/icons/change_password_icon.png', width: 100),
+          Form(
+            key: this.state.formKey,
+            child: ListView(
+              children: [
+                SizedBox(
+                  height: 100,
                 ),
-              ),
-              SizedBox(
-                height: 35,
-              ),
-              Center(
-                child: Container(
-                  child: Text('Cambiar contraseña', 
-                    style: TextStyle(
-                      color: HexColor.fromHex('#3333ff'),
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30
-                    )
+                Center(
+                  child: Container(
+                    child: Image.asset('assets/icons/change_password_icon.png', width: 100),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 35,
-              ),
-              Center(
-                child: Container(
-                  width: this.state.screen.width * .8,
-                  child: Text(
-                    'Ingresa tu nueva contraseña a continuación, es para protección de tu cuenta.', 
-                    style: TextStyle(color: Colors.white, fontSize: 23),
-                    textAlign: TextAlign.center,
-                  ),
+                SizedBox(
+                  height: 35,
                 ),
-              ),
-              SizedBox(
-                height: 35,
-              ),
-              Center(
-                child: Container(
-                  width: this.state.screen.width * .8,
-                  child: Text(
-                    'La contraseña debe tener entre 6 y 10 caracteres y puede incluir letras (mayúsculas o minúsculas), números del 0 al 9 y signos de puntuación.', 
-                    style: TextStyle(color: Colors.white, fontSize: 16),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 35,
-              ),
-              Center(
-                child: Container(
-                  width: this.state.screen.width * .8,
-                  child: TextFormField(
-                    style: TextStyle(color: Colors.white),
-                    textCapitalization: TextCapitalization.none,
-                    decoration: InputDecoration(
-                      labelText: 'Contraseña',
-                      labelStyle: TextStyle(color: Colors.white),
-                      contentPadding: EdgeInsets.all(0),
-                      enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-                      focusColor: Colors.white,
-                      focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white))
-                    )
-                  )
-                ),
-              ),
-              SizedBox(
-                height: 35,
-              ),
-              Center(
-                child: Container(
-                  width: this.state.screen.width * .8,
-                  child: TextFormField(
-                    style: TextStyle(color: Colors.white),
-                    textCapitalization: TextCapitalization.none,
-                    decoration: InputDecoration(
-                      labelText: 'Confirme contraseña',
-                      labelStyle: TextStyle(color: Colors.white),
-                      contentPadding: EdgeInsets.all(0),
-                      enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-                      focusColor: Colors.white,
-                      focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white))
-                    )
-                  )
-                ),
-              ),
-              SizedBox(
-                height: 35,
-              ),
-              Center(
-                child: Container(
-                  width: this.state.screen.width * .8,
-                  child: GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                      padding: EdgeInsets.symmetric(vertical: 25),
-                      decoration: BoxDecoration(
+                Center(
+                  child: Container(
+                    child: Text('Cambiar contraseña', 
+                      style: TextStyle(
                         color: HexColor.fromHex('#3333ff'),
-                        borderRadius: BorderRadius.circular(40)
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30
+                      )
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 35,
+                ),
+                Center(
+                  child: Container(
+                    width: this.state.screen.width * .8,
+                    child: Text(
+                      'Ingresa tu nueva contraseña a continuación, es para protección de tu cuenta.', 
+                      style: TextStyle(color: Colors.white, fontSize: 23),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 35,
+                ),
+                Center(
+                  child: Container(
+                    width: this.state.screen.width * .8,
+                    child: Text(
+                      'La contraseña debe tener entre 6 y 10 caracteres y puede incluir letras (mayúsculas o minúsculas), números del 0 al 9 y signos de puntuación.', 
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 35,
+                ),
+                Center(
+                  child: Container(
+                    width: this.state.screen.width * .8,
+                    child: TextFormField(
+                      controller: this.state.passwordController,
+                      style: TextStyle(color: Colors.white),
+                      textCapitalization: TextCapitalization.none,
+                      decoration: InputDecoration(
+                        labelText: 'Contraseña',
+                        labelStyle: TextStyle(color: Colors.white),
+                        contentPadding: EdgeInsets.all(0),
+                        enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                        focusColor: Colors.white,
+                        focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white))
                       ),
-                      child: Center(
-                        child: Text(
-                          'GUARDAR CONTRASEÑA', 
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'Montserrat',
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18
+                      obscureText: true,
+                      validator: this.state.validPassword,
+                    )
+                  ),
+                ),
+                SizedBox(
+                  height: 35,
+                ),
+                Center(
+                  child: Container(
+                    width: this.state.screen.width * .8,
+                    child: TextFormField(
+                      controller: this.state.confirmPasswordController,
+                      style: TextStyle(color: Colors.white),
+                      textCapitalization: TextCapitalization.none,
+                      decoration: InputDecoration(
+                        labelText: 'Confirme contraseña',
+                        labelStyle: TextStyle(color: Colors.white),
+                        contentPadding: EdgeInsets.all(0),
+                        enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                        focusColor: Colors.white,
+                        focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white))
+                      ),
+                      obscureText: true,
+                      validator: this.state.validConfirmPassword,
+                    )
+                  ),
+                ),
+                SizedBox(
+                  height: 35,
+                ),
+                Center(
+                  child: Container(
+                    width: this.state.screen.width * .8,
+                    child: GestureDetector(
+                      onTap: this.state.loading ? () {} : this.state.submit,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(vertical: 25),
+                        decoration: BoxDecoration(
+                          color: HexColor.fromHex('#3333ff'),
+                          borderRadius: BorderRadius.circular(40)
+                        ),
+                        child: this.state.loading ? 
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'GUARDAR CONTRASEÑA', 
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'Montserrat',
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18
+                                )
+                              ),
+                              Container(
+                                width: 20,
+                                height: 20,
+                                margin: EdgeInsets.only(left: 15),
+                                child: CircularProgressIndicator(
+                                  strokeWidth: 2,
+                                  backgroundColor: Color.fromRGBO(0, 0, 102, 1),
+                                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                                ),
+                              )
+                            ],
                           )
+                        :
+                        Center(
+                          child: Text(
+                            'GUARDAR CONTRASEÑA', 
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18
+                            )
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           )
         ],
       ),
