6f68f2cb53fd552aa21275df7106d919e28f0ef8
Adicto969 <rl.juan666@gmail.com>
2021-02-24 14:24:33 -0600

    remove plugin conectivity

--- lib/Models/AppProviderModel.dart
+++ lib/Models/AppProviderModel.dart
@@ -1,9 +1,7 @@
-import 'package:connectivity/connectivity.dart';
 import 'package:prestaQi/Models/UserToken.dart';
 
 class AppProviderModel {
   
   bool onLine = true;
-  ConnectivityResult typeConnection = ConnectivityResult.none;
   UserToken user = new UserToken();
 }
